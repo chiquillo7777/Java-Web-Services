f5a55fcc5660d5d6058f311407d4e069a4ea1de5
Carlos Jose Chiquillo Lopez <carlos.chiquillo@telusinternational.com>
2020-02-03 11:13:28 -0600

    creating a ut client, implementing PaymentProcessorImplService

---
+++ utclient/src/main/java/com/carloschiquillo/ws/client/PaymentWSClient.java
@@ -0,0 +1,45 @@
+package com.carloschiquillo.ws.client;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.endpoint.Endpoint;
+import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.ws.security.wss4j.WSS4JOutInterceptor;
+import org.apache.wss4j.dom.WSConstants;
+import org.apache.wss4j.dom.handler.WSHandlerConstants;
+
+import com.bharath.ws.soap.PaymentProcessor;
+import com.bharath.ws.soap.PaymentProcessorImplService;
+import com.bharath.ws.soap.PaymentProcessorRequest;
+import com.bharath.ws.soap.PaymentProcessorResponse;
+
+public class PaymentWSClient {
+
+	public static void main(String[] args) {
+		try {
+			PaymentProcessorImplService service = new PaymentProcessorImplService(new URL("http://localhost:8080/javafirstws/paymentProcessor?wsdl"));
+			PaymentProcessor port = service.getPaymentProcessorImplPort();
+			
+			Client client = ClientProxy.getClient(port);
+			Endpoint endpoint = client.getEndpoint();
+			
+			Map<String, Object> props = new HashMap<String, Object>();
+			props.put(WSHandlerConstants.ACTION, WSHandlerConstants.USERNAME_TOKEN);
+			props.put(WSHandlerConstants.USER, "cxf");
+			props.put(WSHandlerConstants.PASSWORD_TYPE, WSConstants.PW_TEXT);
+			
+			WSS4JOutInterceptor wssOut = new WSS4JOutInterceptor(props );
+			
+			PaymentProcessorResponse response = port.processPayment(new PaymentProcessorRequest());
+			System.out.println(response.isResult());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
