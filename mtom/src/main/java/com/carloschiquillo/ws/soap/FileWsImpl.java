8728c1ff891a8ea7a27344b95b7789d0e399cc61
carlos chiquillo <carlosjosechiquillolopez@gmail.com>
2020-02-03 12:04:10 -0600

    STS error will not save work

---
+++ mtom/src/main/java/com/carloschiquillo/ws/soap/FileWsImpl.java
@@ -0,0 +1,19 @@
+package com.carloschiquillo.ws.soap;
+
+import javax.activation.DataHandler;
+
+public class FileWsImpl implements FileWs {
+
+	@Override
+	public void upload(DataHandler attachment) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public DataHandler download() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
