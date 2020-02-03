9188842f98cb68801736657f55f7b8af80b335af
carlos chiquillo <carlosjosechiquillolopez@gmail.com>
2020-02-03 12:18:30 -0600

    adding upload method implementation

--- mtom/src/main/java/com/carloschiquillo/ws/soap/FileWsImpl.java
+++ mtom/src/main/java/com/carloschiquillo/ws/soap/FileWsImpl.java
@@ -1,13 +1,36 @@
 package com.carloschiquillo.ws.soap;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import javax.activation.DataHandler;
 
 public class FileWsImpl implements FileWs {
 
 	@Override
 	public void upload(DataHandler attachment) {
-		// TODO Auto-generated method stub
-
+		
+		
+		try {
+			InputStream inputStream = attachment.getInputStream();
+			OutputStream outputStream = new FileOutputStream(new File("C:\\Users\\carlos.chiquillo\\Documents\\workspace1\\Java-Web-Services\\mtom\\src\\main\\resources\\files\\uploaded\\test.jfif"));
+			byte[] b = new byte[100000];
+			int bytesRead = 0;
+			
+			while((bytesRead = inputStream.read(b))!= -1) {
+				outputStream.write(b, 0, bytesRead);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
 	}
 
 	@Override
