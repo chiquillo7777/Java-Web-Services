5b7ca714ea0dde33c7b52b5a006cf5125f5b904c
carlos chiquillo <carlosjosechiquillolopez@gmail.com>
2020-02-04 00:14:46 -0600

    adding json support with jackson annotations comsumes and produces

--- restws/src/main/java/com/carloschiquillo/restws/PatientService.java
+++ restws/src/main/java/com/carloschiquillo/restws/PatientService.java
@@ -1,17 +1,21 @@
 package com.carloschiquillo.restws;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 import com.carloschiquillo.restws.model.Patient;
 
 import java.util.List;
 
+@Consumes("application/xml,application/json")
+@Produces("application/xml,application/json")
 @Path("/patientservice")
 public interface PatientService {
 	
