e9b0d747d4543579cf4f1385048eca0e229d9e30
carlos chiquillo <carlosjosechiquillolopez@gmail.com>
2020-02-03 23:28:01 -0600

    tes

--- restws/src/main/java/com/carloschiquillo/restws/PatientService.java
+++ restws/src/main/java/com/carloschiquillo/restws/PatientService.java
@@ -1,8 +1,27 @@
 package com.carloschiquillo.restws;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+import com.carloschiquillo.restws.model.Patient;
+
+import java.util.List;
 
 @Path("/patientservice")
 public interface PatientService {
-
+	
+	@Path("/patients")
+	@GET
+	List<Patient> getPatients();
+	
+	@Path("/patients/{id}")
+	@GET
+	Patient getPatient(@PathParam(value = "id") Long id);
+	
+	@Path("/patients")
+	@POST
+	Response createPatient(Patient patient);
 }
--- restws/src/main/java/com/carloschiquillo/restws/PatientServiceImpl.java
+++ restws/src/main/java/com/carloschiquillo/restws/PatientServiceImpl.java
@@ -1,7 +1,12 @@
 package com.carloschiquillo.restws;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import javax.ws.rs.core.Response;
 
 import org.springframework.stereotype.Service;
 
@@ -20,5 +25,29 @@ public class PatientServiceImpl implements PatientService {
 		patient.setId(currentId);
 		patient.setName("John");
 		patients.put(patient.getId(), patient);
+	}
+
+	
+	@Override
+	public List<Patient> getPatients() {
+		Collection<Patient> results = patients.values();
+		List<Patient> response = new ArrayList<>(results);
+		return response;
+	}
+
+	@Override
+	public Patient getPatient(Long id) {
+		
+		
+		
+		return patients.get(id);
+	}
+
+	@Override
+	public Response createPatient(Patient patient) {
+		patient.setId(++currentId);
+		patients.put(patient.getId(), patient);
+		
+		return Response.ok(patient).build();
 	}
 }
