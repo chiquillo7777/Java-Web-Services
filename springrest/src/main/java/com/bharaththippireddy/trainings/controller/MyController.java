3c988adcefccaf77ccb9c8723108690df2f2d7c0
carlos chiquillo <carlosjosechiquillolopez@gmail.com>
2020-02-05 07:35:36 -0600

    ready to test but missing server

---
+++ springrest/src/main/java/com/bharaththippireddy/trainings/controller/MyController.java
@@ -0,0 +1,15 @@
+package com.bharaththippireddy.trainings.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class MyController {
+	@RequestMapping(value="/{name}", method=RequestMethod.GET)
+	public String sayHello(@PathVariable String name) {
+		return "Hello " + name;
+	}
+}
